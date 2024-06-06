180b8e186053c7e9ba3968d8da9d1a053c01ff79
Karl Broman <kbroman@gmail.com>
2024-06-06 15:28:12 -0500

    FixedSizedList: make general again
     - also fix dumb bug in .add()

---
+++ app/src/main/java/org/kbroman/android/polarpvc2/FixedSizedList.kt
@@ -0,0 +1,34 @@
+package org.kbroman.android.polarpvc2
+
+import java.util.ArrayList
+
+// this is to keep all ECG data with ever-growing indexes, but not blow up memory use
+// this contains doubles, for the voltages
+class FixedSizedList<T>(private val maxItems: Int) {
+    private val list = ArrayList<T>()
+    private var offset: Int = 0
+
+    init {
+        offset = 0
+    }
+
+    fun add(value : T) {
+        if (list.size == maxItems) {
+            offset++
+            list.removeFirst()
+        }
+        list.add(value)
+    }
+
+     fun get(index: Int): T {
+        return list.get(index - offset) // gonna be bad if index out of range
+    }
+
+    fun setLast(value: T) {
+        list.set(list.size - 1, value)
+    }
+
+    fun size(): Int = list.size
+
+    fun maxIndex(): Int = list.size + offset
+}
