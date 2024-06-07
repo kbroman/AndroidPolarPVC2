602c04937f39a642262e121d0bdfa84cdd59f788
Karl Broman <kbroman@gmail.com>
2024-06-07 10:18:36 -0500

    Disable writing data attempts; seems super complicated

--- app/src/main/java/org/kbroman/android/polarpvc2/MainActivity.kt
+++ app/src/main/java/org/kbroman/android/polarpvc2/MainActivity.kt
@@ -32,8 +32,8 @@ private lateinit var binding: ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private var context: Context = this.getApplicationContext()
-    var filePath: String = context.getExternalFilesDir(null).toString() + "/PolarECGdata"
+    //private var context: Context = this.getApplicationContext()
+    //var filePath: String = context.getExternalFilesDir(null).toString() + "/PolarECGdata"
     private var deviceId: String = "D45EC729"
     private var ecgDisposable: Disposable? = null
     private var deviceConnected = false
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     public val pd: PeakDetection = PeakDetection()
-    public val wd: WriteData = WriteData(filePath)
+    //public val wd: WriteData = WriteData(filePath)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -258,6 +258,9 @@ class MainActivity : AppCompatActivity() {
         toast.show()
     }
 
+
+
+
     fun streamECG() {
         val isDisposed = ecgDisposable?.isDisposed ?: true
         if (isDisposed) {
@@ -270,7 +273,7 @@ class MainActivity : AppCompatActivity() {
                         Log.i(TAG, "ecg update")
 
                         pd.processData(polarEcgData)  // PeakDetection -> find_peaks
-                        if(isRecording) wd.writeData(polarEcgData)
+                        //if(isRecording) wd.writeData(polarEcgData)
 
                         if(pd.rrData.size() > 1) {
                             val hr_bpm = Math.round(60.0 / pd.rrData.average())
