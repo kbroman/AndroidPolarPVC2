c9fc76de25a087c1f963e1024eae64b6d7bcc16a
Karl Broman <kbroman@gmail.com>
2024-06-07 21:10:34 -0500

    Layout for android plots

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 
 dependencies {
+    implementation("com.androidplot:androidplot-core:1.5.10")
     implementation("com.github.polarofficial:polar-ble-sdk:5.5.0")
     implementation("io.reactivex.rxjava3:rxjava:3.1.6")
     implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
